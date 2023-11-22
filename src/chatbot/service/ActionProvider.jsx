3073032ced4e06d5e9e782d58452c083f31ee467
Rohitbhojwani <bhojwanirs@rknec.edu>
2023-11-22 19:55:58 +0530

    Added files

---
+++ src/chatbot/service/ActionProvider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ActionProvider = ({ children }) => {
+  
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+          
+          },
+        });
+      })}
+    </div>
+  );
+};
+
+export default ActionProvider;
